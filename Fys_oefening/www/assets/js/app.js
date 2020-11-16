3357093dd3e0185dcb7bcd4e8b47c442157cb3a6
Malloum003 <mohammed.malloul@hva.nl>
2020-11-16 11:46:05 +0100

    Profiel bewerken optie toegevoegd

--- Fys_oefening/www/assets/js/app.js
+++ Fys_oefening/www/assets/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function (qualifiedName, value) {
     $(".hamburgermenu").on("click", function (e) {
         e.preventDefault();
         var x = document.getElementById("responsivemenu");
@@ -48,6 +48,42 @@ $(document).ready(function () {
         }
     })
 
+    $('.gegevens_bewerken').on("click", function (bewerken) {
+        bewerken.preventDefault();
+        var readonlycheck = document.getElementById("profiel_input1").hasAttribute("readonly");
+
+        if (readonlycheck) {
+            //$(".tekstbox").removeAttr("readonly");
+            document.getElementById("profiel_input1").readOnly = false;
+            document.getElementById("profiel_input2").readOnly = false;
+            document.getElementById("profiel_input3").readOnly = false;
+            document.getElementById("profiel_input4").readOnly = false;
+            document.getElementById("profiel_input5").readOnly = false;
+            document.getElementById("profiel_input6").readOnly = false;
+            document.getElementById("profiel_input7").readOnly = false;
+            document.getElementById("profiel_input8").readOnly = false;
+            document.getElementById("gegevens_opslaan").style.display = "block";
+            document.getElementById("gegevens_bewerken").style.display = "none";
+
+            console.log("false");
+        }else{
+            //$(".tekstbox").setAttribute("readonly") ;
+            document.getElementById("profiel_input1").readOnly = true;
+            document.getElementById("profiel_input2").readOnly = true;
+            document.getElementById("profiel_input3").readOnly = true;
+            document.getElementById("profiel_input4").readOnly = true;
+            document.getElementById("profiel_input5").readOnly = true;
+            document.getElementById("profiel_input6").readOnly = true;
+            document.getElementById("profiel_input7").readOnly = true;
+            document.getElementById("profiel_input8").readOnly = false;
+            document.getElementById("gegevens_bewerken").style.display = "block";
+            document.getElementById("gegevens_opslaan").style.display = "none";
+            console.log("true");
+        }
+
+
+
+    })
 });
 
 //onclick="location.href='Profiel_Overzicht.html'"
