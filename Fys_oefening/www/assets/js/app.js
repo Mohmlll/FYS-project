033f9b422ef47d07d6ff8dab35a548b2b4e849e1
Dylan Kroon <dylan.kroon@hva.nl>
2020-11-27 14:19:03 +0100

    Home toegevoegd aan het menu

--- Fys_oefening/www/assets/js/app.js
+++ Fys_oefening/www/assets/js/app.js
@@ -24,6 +24,7 @@ $(document).ready(function () {
         "<div class=\"logomemu\"><a href=\"index.html\"><img alt=\"Corendon TravelBud\" src=\"assets/img/Logo_website.png\"></a>" +
         "</div>" +
         "<div class=\"menu_groot\">" +
+        "    <a href=\"index.html\">Home</a>" +
         "    <a href=\"over-ons.html\">Over ons</a>" +
         "    <a class=\"js-logged-out\" href=\"log-in.html\">Aanmelden</a>" +
         "    <a class=\"js-logged-in\" href=\"profiel-overzicht.html\">Profiel</a>" +
