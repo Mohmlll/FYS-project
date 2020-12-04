b74fd5d8f0bd8cb3b34197c2253630c14842fa6e
Malloum003 <mohammed.malloul@hva.nl>
2020-12-04 12:55:12 +0100

    tags zijn werkend met database, checkbox edit moet nog toegepast

--- Fys_oefening/www/assets/js/pages/tagbuttons.js
+++ Fys_oefening/www/assets/js/pages/tagbuttons.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
-    $(".tag_div label").click(function (tag) {
-        tag.preventDefault();
-        $(this).find("i").toggleClass("fas fa-plus fas fa-check");
-    })
-
+    // $(".tag_div label").click(function (tag) {
+    //     tag.preventDefault();
+    //
+    //     $(this).find("i").toggleClass("fas fa-plus fas fa-check");
+    //     $(".tag_button").toggleClass()
+    // })
+    //
+    //
 
 });
