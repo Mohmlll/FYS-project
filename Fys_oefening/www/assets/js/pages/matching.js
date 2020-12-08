d304fea6c2e9023714b560ad71d7bcc686c206b5
Dylan Kroon <dylan.kroon@hva.nl>
2020-12-08 09:01:51 +0100

    Matching begin

--- Fys_oefening/www/assets/js/pages/matching.js
+++ Fys_oefening/www/assets/js/pages/matching.js
@@ -16,34 +16,104 @@ $(document).ready(function () {
         return template.firstElementChild;
     }
 
-
-    var appendVoornaam;
-    var appendAchternaam;
-
     FYSCloud.API.queryDatabase(
-        "SELECT voornaam, achternaam, gebruikerid, profiel_foto FROM gebruiker_profiel"
+        "SELECT * FROM interesse WHERE idgebruiker = ?",
+        [sessionStorage.getItem("userId")]
     ).done(function (data) {
-        var noOfTemplates = data.length;
-        console.log(noOfTemplates)
-        for (let i = 0; i < noOfTemplates; i++) {
-            console.log(data[i])
-            var postId = data[i]['gebruikerid'];
-            var photoUrl = "https://i.imgur.com/b5TbCKd.png";
-            if (data[i]["profiel_foto"] === "foto") {
-                photoUrl = "https://dev-is106-3.fys.cloud/uploads/" + postId + ".png";
-                console.log(data[i]["profiel_foto"])
+        console.log(data);
+        var backpacker = data[0]["backpacker"];
+        var explorer = data[0]["explorer"];
+        var sportieveling = data[0]["sportieveling"];
+        var relaxer = data[0]["relaxer"];
+        var partygoer = data[0]["partygoer"];
+        var wintersport = data[0]["wintersport"];
+        var tropisch = data[0]["tropisch"];
+        var resort = data[0]["resort"];
+
+        FYSCloud.API.queryDatabase(
+            "SELECT * FROM interesse"
+        ).done(function (data) {
+            console.log(data);
+            var maxScore = 10;
+            var scoreArray = []
+            for (let i = 0; i < data.length; i++) {
+                console.log(data[i]["idgebruiker"] + " " + sessionStorage.getItem("userId"))
+                if (data[i]["idgebruiker"] !== sessionStorage.getItem("userId")) {
+                    var puntentelling = 0;
+                    if (data[i]["backpacker"] === backpacker) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["explorer"] === explorer) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["sportieveling"] === sportieveling) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["relaxer"] === relaxer) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["partygoer"] === partygoer) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["wintersport"] === wintersport) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["tropisch"] === tropisch) {
+                        puntentelling += maxScore;
+                    }
+                    if (data[i]["resort"] === resort) {
+                        puntentelling += maxScore;
+                    }
+                    scoreArray.push([puntentelling, data[i]["idgebruiker"]]);
+                } else {
+                    console.log("hallo")
+                }
+
             }
-            var img = new Image()
-            img.src = photoUrl;
-            appendVoornaam = data[i]['voornaam']
-            appendAchternaam = data[i]['achternaam']
-            console.log(appendVoornaam, appendAchternaam, photoUrl,postId)
-            let costumElement = makeAnElement(appendVoornaam, appendAchternaam, photoUrl)
-            nieuwePost.appendChild(costumElement);
-        }
-        //TODO: appendPhoto,postId zijn undefined
+            console.log(scoreArray);
+            scoreArray.sort(function (a, b) {return b[0] - a[0]} );
+            var top5 = scoreArray.slice(0, 3)
+            console.log(top5)
+
+            var appendVoornaam;
+            var appendAchternaam;
+
+
+            var noOfTemplates = 3;
+            console.log(noOfTemplates)
+
+            for (let i = 0; i < noOfTemplates; i++) {
+                console.log(top5[i][1]);
+                FYSCloud.API.queryDatabase(
+                    "SELECT voornaam, achternaam, gebruikerid, profiel_foto FROM gebruiker_profiel WHERE gebruikerid = ?",
+                    [top5[i][1]]
+                ).done(function (data) {
+                    console.log(data[0]);
+                    console.log(top5[i][1]);
+                    var postId = top5[i][1];
+                    var photoUrl = "https://i.imgur.com/b5TbCKd.png";
+                    if (data[0]["profiel_foto"] === "foto") {
+                        photoUrl = "https://dev-is106-3.fys.cloud/uploads/" + postId + ".png";
+                        console.log(data[0]["profiel_foto"]);
+                    }
+                    var img = new Image();
+                    img.src = photoUrl;
+                    appendVoornaam = data[0]['voornaam'];
+                    appendAchternaam = data[0]['achternaam'];
+                    console.log(appendVoornaam, appendAchternaam, photoUrl, postId);
+
+                    let costumElement = makeAnElement(appendVoornaam, appendAchternaam, photoUrl);
+                    nieuwePost.appendChild(costumElement);
+                }).fail(function (reason) {
+                    console.log(reason);
+                    console.log("fout");
+                })
+            }
+        }).fail(function (reason) {
+            console.log(reason);
+        })
     }).fail(function (reason) {
         console.log(reason);
-        console.log("fout");
     })
+
 })
