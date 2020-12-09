4a92902418149826d4d7fbc3981443187163fba5
Marvin <marvin.hesselmans@hva.nl>
2020-12-09 15:44:05 +0100

    About Us (with functional translation button)

--- Fys_oefening/www/assets/js/pages/translate.js
+++ Fys_oefening/www/assets/js/pages/translate.js
@@ -2,8 +2,6 @@
     new google.translate.TranslateElement({
         pageLanguage: 'nl',
         layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-        autoDisplay: false,
-        includedLanguages: '',
     }, 'google_translate_element');
 }
 
