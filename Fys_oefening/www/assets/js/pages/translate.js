21429f4e15ba68e3fa03a8506dda42f2006a2e8b
Marvin <marvin.hesselmans@hva.nl>
2020-12-09 14:50:15 +0100

    Translate Script ready

---
+++ Fys_oefening/www/assets/js/pages/translate.js
@@ -0,0 +1,10 @@
+    function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        pageLanguage: 'nl',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false,
+        includedLanguages: '',
+    }, 'google_translate_element');
+}
+
+
